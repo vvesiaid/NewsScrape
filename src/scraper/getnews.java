5f7496a1d926e2307c77ce2b68c02fd89ae39e8d
Vesse <vesse.vesiaid@eesti.ee>
2015-11-28 17:44:28 +0200

    All the files here now

---
+++ src/scraper/getnews.java
@@ -0,0 +1,18 @@
+package scraper;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class getnews {
+	public static void main(String[] args) throws IOException {
+		Document document = Jsoup.connect("http://www.aripaev.ee").get();
+		Elements pealkirjad = document.select("h3.art-title");
+		for (Element element : pealkirjad){
+			System.out.println(element.text());
+		}}
+
+}
